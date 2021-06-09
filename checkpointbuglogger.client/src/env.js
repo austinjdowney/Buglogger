7b9ad925570bf1249e8d3667d5d585d6563fcd4e
austinjdowney <austinjdowney@gmail.com>
2021-06-08 21:33:14 -0600

    testing

--- checkpointbuglogger.client/src/env.js
+++ checkpointbuglogger.client/src/env.js
@@ -1,5 +1,5 @@
 export const dev = window.location.origin.includes('localhost')
-export const baseURL = dev ? 'https://localhost:5001' : ''
+export const baseURL = dev ? 'http://localhost:3000' : ''
 export const domain = 'austins-personal.us.auth0.com'
 export const audience = 'https://buglogger.com'
 export const clientId = 'wZZeL3naLABhfFo2uaTCxMFrUPdFoOGA'
